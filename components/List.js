5fbf4f27b5e7cafd534e57cb905dd08eec495e16
Remy Silanto <remy.silanto99@gmail.com>
2022-08-29 18:32:49 +0300

    Task-B-complete

--- components/List.js
+++ components/List.js
@@ -2,16 +2,20 @@ import {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
+const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
 
 const List = () => {
-  const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const [mediaArray, setMediaArray] = useState([]);
   const loadMedia = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(apiUrl + 'media?limit=10');
       const json = await response.json();
       console.log(json);
-      setMediaArray(json);
+      const allMediaData = json.map(async (mediaItem) => {
+        const response = await fetch(apiUrl + 'media/' + mediaItem.file_id);
+        return await response.json();
+      });
+      setMediaArray(await Promise.all(allMediaData));
     } catch (error) {
       console.log('something went wrong with fetching media', error);
     }
