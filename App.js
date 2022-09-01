f9b3b167c1b44a643d4b26e4db26d593a24b9ece
Remy Silanto <remy.silanto99@gmail.com>
2022-09-01 19:31:35 +0300

    UX-Week1

--- App.js
+++ App.js
@@ -1,22 +1,70 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  ImageBackground,
+  Platform,
+} from "react-native";
 
 import List from './components/List';
 
 const App = () => {
   return (
-      <SafeAreaView style={styles.container}>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <SafeAreaView style={styles.container}>
+          <ImageBackground
+            source={require('./assets/cat.jpg')}
+            resizeMode='cover'
+            style={styles.backgroundImage}
+            imageStyle={styles.imageRounding}
+          >
+           <Text style={styles.imageText}>Homeless Kittens</Text>
+          </ImageBackground>
+        </SafeAreaView>
         <List />
-        <StatusBar style="auto" />
       </SafeAreaView>
+        <StatusBar style="auto" />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#1b1b21',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
   container: {
-    display: 'flex',
-    alignItems: 'flex-start',
+    height: 220,
     backgroundColor: 'white',
+    margin: 4,
+    borderRadius: 6,
+    borderBottomRightRadius: 50,
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  imageRounding: {
+    borderRadius: 6,
+    borderBottomRightRadius: 50,
+  },
+  Images: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  imageText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    backgroundColor: '#1f6dff',
+    marginTop: 150,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    position: 'absolute'
   },
 });
 
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,10 +1,16 @@
-import {Image, Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
 import PropTypes from 'prop-types';
 import {vh} from 'react-native-expo-viewport-units';
 
 const ListItem = (props) => {
   return (
-    <TouchableOpacity style={styles.luokkaClass}>
+    <TouchableOpacity style={styles.mainPostContainer}>
       <View style={styles.leftContainer}>
         <Image
           style={styles.images}
@@ -13,44 +19,49 @@ const ListItem = (props) => {
       </View>
       <View style={styles.rightContainer}>
         <Text style={styles.titles}>{props.singleMedia.title}</Text>
-        <Text>{props.singleMedia.description}</Text>
+        <Text style={styles.texts}>{props.singleMedia.description}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  luokkaClass: {
+  mainPostContainer: {
     display: 'flex',
-      flexWrap: 'nowrap',
-      flexDirection: 'row',
-      backgroundColor: 'lightgray',
-      margin: 16,
-      borderTopWidth: 13,
-      borderBottomWidth: 13,
-      borderColor: 'lightgray',
-      borderStyle: 'solid',
-      borderRadius: 10,
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    height: vh(17),
+    margin: 16,
+    borderTopWidth: 13,
+    borderBottomWidth: 13,
+    borderColor: '#2d2d38',
+    borderStyle: 'solid',
+    borderRadius: 10,
   },
   leftContainer: {
-    height: vh(40),
-      width: '40%',
-      justifyContent: 'center',
-      padding: 10,
-      backgroundColor: 'lightgray',
+    width: '50%',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#2d2d38',
   },
   rightContainer: {
-    width: '60%',
-      backgroundColor: 'lightgray',
-      padding: 10,
+    width: '50%',
+    backgroundColor: '#2d2d38',
+    padding: 10,
   },
   titles: {
+    color: '#ffffff',
     fontWeight: 'bold',
-      fontSize: 20,
+    fontSize: 14,
+  },
+  texts: {
+    color: '#ffffff',
+    fontSize: 12,
   },
   images: {
     height: '100%',
-      borderRadius: 5,
+    borderRadius: 5,
+    borderBottomLeftRadius: 50,
     // height: vh(20),
     // width: vh(20),
   },
